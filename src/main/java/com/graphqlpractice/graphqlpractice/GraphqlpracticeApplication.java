7204ce1e1eb85476b7655f73214ed917d628df33
ivanarieldmc <ivanarieldmc@gmail.com>
2021-02-24 16:45:33 -0300

    First commit

---
+++ src/main/java/com/graphqlpractice/graphqlpractice/GraphqlpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.graphqlpractice.graphqlpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlpracticeApplication.class, args);
+	}
+
+}
